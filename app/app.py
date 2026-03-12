8dd61757b38ed880bbc0a6e88a64fe68e42979e9
Deadlock6 <ddd222222222222222@gmail.com>
2026-03-12 17:44:26 +0500

    routing(

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+@app.route('/greet/<name>')
+def greet(name):
+  return f'Hello, {name}!'
+
+if __name__ == '__main__':
+  app.run(debug=True)
