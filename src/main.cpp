269014a236dcae24c001006862cc5b69d4f32456
Deadlock6 <ddd222222222222222@gmail.com>
2026-03-26 17:27:15 +0500

    добавлено измерение электропроводности

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,39 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <GravityTDS.h>
 
 // Consants
 const unsigned char ONE_WIRE_BUS = 4;
 DeviceAddress temperature_sensor = {0x28, 0x73, 0xF6, 0x45, 0xD4, 0xC7, 0x6B, 0x9C};
 
+const unsigned char TDS_PIN = 16; 
+
 // variables
 unsigned long previousTime = millis();
+double current_temp;
 
+double ec;
+
+// objects
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+GravityTDS tds;
+
 
 
 
 void setup() {
   Serial.begin(9600);
   sensors.begin();
+
+// tds meter initialization
+  tds.setPin(TDS_PIN);
+  tds.setAref(3.3);     
+  tds.setAdcRange(4095); 
+  tds.begin();
+
+
 }
 
 void loop() {
@@ -26,8 +43,14 @@ void loop() {
     Serial.print("Requesting Temperatures...");
     sensors.requestTemperatures();
     Serial.print("done :)\n");
+    current_temp = sensors.getTempC(temperature_sensor);
+    Serial.println(current_temp);
 
-    Serial.println(sensors.getTempC(temperature_sensor));
+    tds.setTemperature(current_temp);
+    ec = tds.getTdsValue() / 0.5;
+
+    Serial.print("EC: ");
+    Serial.print(ec);
   }
 }
 
